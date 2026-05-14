ad2d6bff405aa05e63327827a0975257264857fa
yiyunkj999 <117581523+yiyunkj999@users.noreply.github.com>
2026-05-15 01:59:39 +0800

    Update tongji.js

--- tongji.js
+++ tongji.js
@@ -1,24 +1,22 @@
-    //google analytics
-    (function() {
-      const domain = 'analytics.123101.xyz/o9o4';
-      const tongjiid = new URL(document.currentScript.src).searchParams.get('id');
-      var script = document.createElement('script');
-      script.async = true;
-      script.src = `//${domain}/gtm.js?id=${tongjiid}`;
-      document.head.appendChild(script);
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', tongjiid, {transport_url: domain});
-    })();
+//google analytics
+(function() {
+    const domain = 'analytics.123101.xyz/o9o4';
+    const tongjiid = new URL(document.currentScript.src).searchParams.get('id');
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = `//${domain}/gtm.js?id=${tongjiid}`;
+    document.head.appendChild(script);
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', tongjiid, {transport_url: domain});
+})();
     
-    //baiud push
-    (function(){
-      setTimeout(()=>{
-        const s = document.createElement('script');
-        s.src = location.protocol === 'https:' 
-          ? 'https://zz.bdstatic.com/linksubmit/push.js' 
-          : 'http://push.zhanzhang.baidu.com/push.js';
-        document.body.appendChild(s);
-      }, 500);
-    })();
+//baiud push
+(function(){
+    setTimeout(()=>{
+    const s = document.createElement('script');
+    s.src = location.protocol === 'https:' ? 'https://zz.bdstatic.com/linksubmit/push.js' : 'http://push.zhanzhang.baidu.com/push.js';
+    document.body.appendChild(s);
+    }, 500);
+})();
